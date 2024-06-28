e7c26cfba2e064ad21b1804c39f849ba8f00d9c6
Raiff Lucena <raiffb@gmail.com>
2024-06-28 06:54:49 -0300

    Define Media type directly within CategoryCard component to fix type error

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,11 +1,24 @@
 "use client"
 
 import React from 'react'
-
-import classes from './index.module.scss'
 import Link from 'next/link'
+import classes from './index.module.scss'
 import { Category } from '../../../../payload/payload-types'
 import { useFilter } from '../../../_providers/Filter'
+
+type Media = {
+  id: string
+  alt: string
+  caption?: any
+  updatedAt?: string
+  createdAt?: string
+  url?: string
+  filename?: string
+  mimeType?: string
+  filesize?: number
+  width?: number
+  height?: number
+}
 
 type CategoryCardProps = {
     category: Category
@@ -15,14 +28,16 @@ const CategoryCard = ({ category }: CategoryCardProps) => {
     const media = category.media as Media
     const { setCategoryFilters } = useFilter()
 
-    return <Link
-              href="/products"
-              className={classes.card}
-              style={{ backgroundImage: `url(${media.url})` }}
-              onClick={() => setCategoryFilters([category.id])}
-            >
-        <p className={classes.title}>{category.title}</p>
-    </Link>
+    return (
+        <Link
+            href="/products"
+            className={classes.card}
+            style={{ backgroundImage: `url(${media.url})` }}
+            onClick={() => setCategoryFilters([category.id])}
+        >
+            <p className={classes.title}>{category.title}</p>
+        </Link>
+    )
 }
 
 export default CategoryCard
