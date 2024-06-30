4eb945e41cd4c166581dd15956d29446452630dc
Raiff Lucena <raiffb@gmail.com>
2024-06-29 21:02:40 -0300

    fix: resolve ESLint and Prettier issues, organize imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  ignorePatterns: ['**/payload-types.ts', '**/build/**', '**/dist/**'],
+  plugins: ['prettier', 'simple-import-sort'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-console': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'function-paren-newline': ['error', 'consistent'],
   },
 }
