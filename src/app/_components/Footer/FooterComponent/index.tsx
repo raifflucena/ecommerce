b150be5abc92901dd690c999cbaa770bea32a176
Raiff Lucena <raiffb@gmail.com>
2024-06-30 09:52:56 -0300

    fix: handle null values in FooterComponent to prevent runtime errors

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,20 +1,24 @@
-'use client'
+'use client';
 
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
-import { Gutter } from '../../Gutter'
-import { Footer, Media } from '../../../../payload/payload-types'
-import { Button } from '../../Button'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants';
+import { Gutter } from '../../Gutter';
+import { Footer, Media } from '../../../../payload/payload-types';
+import { Button } from '../../Button';
 
-import classes from './index.module.scss'
+import classes from './index.module.scss';
 
-const FooterComponent = ({ footer }: { footer: Footer }) => {
-  const pathname = usePathname()
-  const navItems = footer?.navItems || []
+const FooterComponent = ({ footer }: { footer: Footer | null }) => {
+  const pathname = usePathname();
+  const navItems = footer?.navItems || [];
+
+  if (!footer) {
+    return null;
+  }
 
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
@@ -29,27 +33,22 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
                 height={36}
                 className={classes.icon}
               />
-
               <h5 className={classes.title}>{inclusion.title}</h5>
               <p>{inclusion.description}</p>
             </li>
           ))}
         </ul>
       </Gutter>
-
       <div className={classes.footer}>
         <Gutter>
           <div className={classes.wrap}>
             <Link href="/">
               <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
             </Link>
-
             <p>{footer.copyright}</p>
-
             <div className={classes.socialLinks}>
               {navItems.map(item => {
-                const icon = item?.link?.icon as Media
-
+                const icon = item?.link?.icon as Media;
                 return (
                   <Button
                     key={item.link.label}
@@ -66,14 +65,14 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
                       className={classes.socialIcon}
                     />
                   </Button>
-                )
+                );
               })}
             </div>
           </div>
         </Gutter>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
